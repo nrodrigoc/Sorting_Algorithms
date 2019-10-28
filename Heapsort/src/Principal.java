e08623c52f32e2576674a97c829c090f9ddea14f
Nathan Rodrigo <nathan--rodrigo@hotmail.com>
2019-10-28 19:20:20 -0300

    leitura de instancias numericas

--- Heapsort/src/Principal.java
+++ Heapsort/src/Principal.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class Principal {
 
 	public static void printArray(int ar[]) {
@@ -6,31 +12,61 @@ public class Principal {
 		}
 	}
 	
-	
 	public static void main(String[] args) {
-		
 		if(args.length > 1) {
 			System.out.println("Ta errado isso ai irmao");
 			System.exit(-1);
 		}
 		
 		String filename = args[0];
-		System.out.println(filename);
+		
+		String directory = Principal.class.getResource(filename).toString();
+		
+		if(directory != null)
+			directory = directory.replace("file:/", "");
+		else {
+			System.out.println("Arquivo inexistente");
+			System.exit(-1);
+		}
+			
+		
+		System.out.println("Diretorio do arquivo: \n" + directory);
+		System.out.println();
+		
+		
+		System.out.println("Instancia numerica: \n");
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(directory));
+			
+			while(br.ready()){
+		        String linha = br.readLine();
+		        //int i = Integer.parseInt(linha);
+		        System.out.println(linha);
+		     }
+		     br.close();
+			
+		} catch (IOException e) {
+			System.out.println("Arquivo nao encontrado!");
+			e.printStackTrace();
+		}
+		
+		//String filename = args[0];
+		//System.out.println(filename);
 		
         /*for(String arg : args)
             System.out.println(arg) ;*/
 		
         
         
-        int ar[] = {7, 2, 5, 3, 0}; 
-        System.out.println("O array original eh: ");
+       /* int ar[] = {7, 2, 5, 3, 0}; 
+        System.out.println("\nO array original eh: ");
         printArray(ar);
         
         
         Heapsort ob = new Heapsort(ar); 
         ar = ob.getHeap();
         System.out.println("\nO array organizado eh: ");
-        printArray(ar);
+        printArray(ar);*/
 	}
 
 }
